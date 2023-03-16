6f69fabec2b0d1efce35bbf81c4fdababf7dbce8
Orlando Eduardo Pereira <orlando-eduardo@hotmail.com>
2023-03-16 11:05:51 -0300

    refactor code and write new cep list in file.txt

--- lib/filter_cep_txt.dart
+++ lib/filter_cep_txt.dart
@@ -2,14 +2,26 @@ import 'dart:convert';
 import 'dart:io';
 
 class FilterCepTxt {
-  List<String?> filterCep() {
-    final file = File('ceps.txt');
-    final text = file.readAsStringSync();
+  Future<List<String?>> filterCep() async {
+    final text = readFile();
 
     final cepPattern = RegExp(r'\b\d{5}-\d{3}\b');
     final ceps = cepPattern.allMatches(text).map((match) => match.group(0)).toList();
 
-    print(jsonEncode(ceps));
+    await saveFileString(jsonEncode(ceps));
     return ceps;
+  }
+
+  String readFile() {
+    final file = File('ceps.txt');
+    return file.readAsStringSync();
+  }
+
+  Future<void> saveFileString(String value) async {
+    var file = File('ceps_filtered.txt');
+    var sink = file.openWrite();
+    sink.write('Hello, world!');
+    await sink.close();
+    print(value);
   }
 }
--- test/filter_cep_txt_test.dart
+++ test/filter_cep_txt_test.dart
@@ -2,8 +2,8 @@ import 'package:filter_cep_txt/filter_cep_txt.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('filter cep', () {
-    final result = FilterCepTxt().filterCep();
+  test('filter cep', () async {
+    final result = await FilterCepTxt().filterCep();
     expect(result, isA<List<String?>>());
     expect(result.isEmpty, false);
   });
